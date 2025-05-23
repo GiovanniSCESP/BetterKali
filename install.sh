a2c63458daa4dcf14fda4579d6961a99ac5f8ca0
GiovanniSCESP <giovanniandre2003@gmail.com>
2025-05-23 17:03:54 +0100

    update

--- install.sh
+++ install.sh
@@ -237,11 +237,11 @@ function fzfc() {
 }
 
 function y() {
-  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-  yazi "$@" -- cwd-file="$tmp"
-  IFS= read -r -d '' cwd < "$tmp"
-  [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd - - "$cwd"
-  rm -f -- "$tmp"
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
 }
 
 function zle_fuck_tmux() {
